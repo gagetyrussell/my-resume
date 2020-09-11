a820f8608ab2b05a05a64abd119f6596a0537e82
gagetyrussell <gagetyrussell@gmail.com>
2020-09-11 13:29:43 -0500

    index page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ export default () =>
     <h1>Index Page</h1>
     <h2>Pages</h2>
     <ul>
-      <li><Link to="/resume/overview">/resume/overview</Link></li>
+      <li><h3>Curriculum Vitae</h3></li>
+      <ul>
+        <li><Link to="/resume/overview">/resume/overview</Link></li>
+      </ul>
+      <li><h3>Tools</h3></li>
+      <ul>
+        <li><Link to="/tools/pivotReporter">/tools/pivotReporter</Link></li>
+      </ul>
     </ul>
   </>
