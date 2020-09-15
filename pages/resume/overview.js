02bca490142f5f573ac2d489938124659090fdf4
gagetyrussell <gagetyrussell@gmail.com>
2020-09-14 20:41:44 -0500

    overview additions

--- pages/resume/overview.js
+++ pages/resume/overview.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Button, Timeline, Breadcrumb  } from 'antd';
 import Plot from 'react-plotly.js';
 import styles from './resume.less';
+import OverViewTimeline from '../../components/overViewTimeline/index.js';
+
 
 const today = new Date()
 const current_date = today.toISOString();
@@ -51,137 +53,52 @@ const volunteer_data = [
 
 const work_data = [...role_data, ...volunteer_data]
 
-const awards = [
+const skill_data = [
+  {"skill": "python", "start": "2015-01-01", "end": "current", "proficiency": "Expert"},
+  {"skill": "R", "start": "2017-01-01", "end": "current", "proficiency": "Advanced"},
+  {"skill": "MATLAB", "start": "2015-01-01", "end": "current", "proficiency": "Advanced"},
+  {"skill": "Mathematica", "start": "2014-01-01", "end": "2015-01-01", "proficiency": "Intermediate"},
+  {"skill": "SQL", "start": "2017-01-01", "end": "current", "proficiency": "Advanced"},
+  {"skill": "NoSQL", "start": "2019-01-01", "end": "current", "proficiency": "Advanced"},
+  {"skill": "ReactJS", "start": "2019-01-01", "end": "current", "proficiency": "Intermediate"},
+]
+
+const award_data = [
   {"color": "#CBC500", "award": "Honorary Professor", "organization": "Society of Petroleum Engineers", "date": '2017-04-06'},
   {"color": "#CBC500", "award": "Outstanding New Tutor of the Year Runner-Up", "organization": "University of Oklahoma College Action Program", "date": '2016-04-06'},
-  {"color": "#CBC500", "award": "Honorary Professor", "organization": "Society of Petroleum Engineers", "date": '2017-04-06'},
+  {"color": "#CBC500", "award": "HSSE-SR Studen Challenge 2nd Place", "organization": "Society of Petroleum Engineers", "date": '2017-04-20'},
+  {"color": "#CBC500", "award": "President's Honor Roll", "organization": "University of Oklahoma", "date": '2015-12-30'},
+  {"color": "#CBC500", "award": "Dean's Honor Roll", "organization": "University of Oklahoma", "date": '2016-06-30'},
+  {"color": "#CBC500", "award": "Dean's Honor Roll", "organization": "University of Oklahoma", "date": '2016-12-30'},
+  {"color": "#CBC500", "award": "Dean's Honor Roll", "organization": "University of Oklahoma", "date": '2017-06-30'},
+  {"color": "#CBC500", "award": "Dean's Honor Roll", "organization": "University of Oklahoma", "date": '2017-12-30'},
+  {"color": "#CBC500", "award": "Dean's Honor Roll", "organization": "University of Oklahoma", "date": '2018-06-30'},
 ]
 
 const colors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B603A5', '#FF97FF', '#FECB52', '#2DA005']
 
 class Overview extends React.Component {
   render() {
-    // replace "current" with now
-    work_data.map((d) => {
-        d.end = d.end.toString().replace('current', current_date);
-    });
-    // turn date strings into dates
-    work_data.map((d) => {
-        d.start = new Date(d.start)
-        d.end = new Date(d.end)
-        return d
-    });
-    // find the max date
-    let maxDate = new Date(Math.max.apply(null, work_data.map(function(d) {
-      return d.end;
-    })));
-    // find the min date
-    let minDate = new Date(Math.min.apply(null, work_data.map(function(d) {
-      return new Date(d.start);
-    })));
-    // find the mid date
-    let midDate = new Date((minDate.getTime() + maxDate.getTime()) / 2);
-    // sort by start date
-    work_data.sort((a, b) => b.start - a.start)
 
-    let companies = work_data.map((d) => {
-      return (
-        d.company
-      )
-    })
-    let starts = work_data.map((d, i) => {
-      return (
-        d.start
-      )
-    })
-    let legendgroups = []
-    let showlegend_bool = true
+    let g = ['', '', '']
+    console.log('og', g)
+    g.splice(0, 1, 'hello')
+    console.log('11', g)
 
-    // define traces
-    let role_traces = work_data.map((d, i) => {
-      // determine if legend already exists
-      if (legendgroups.includes(d.category)) {
-        showlegend_bool = false
-      } else {
-        showlegend_bool = true
-        legendgroups.push(d.category)
-      }
+    let gr = ['', '', '']
+    console.log('og2', gr)
+    gr.splice(gr.length-1, 1, 'hello')
+    console.log('112', gr)    // g.splice(1, 0, 'hey')
+    // console.log('10', g)
 
-      //determine which side text should float
-      let start_mid = Math.abs(d.start - midDate)
-      let end_mid = Math.abs(d.end - midDate)
-      let text = [d.company + " ", ""]
-      let textposition = "left"
-      if (start_mid > end_mid) {
-        text = ["", " " + d.company]
-        textposition = "right"
-      } else {
-        text = [d.company + " ", ""]
-        textposition = "left"
-      }
-
-      return (
-        {
-          x: [d.start, d.end],
-          y: [i, i],
-          text: text,
-          customdata: [d.position, d.position],
-          textposition: textposition,
-          textfont: {
-            size: 12,
-          },
-          type: 'lines',
-          mode: 'lines+text',
-          legendgroup: d.category,
-          name: d.category,
-          showlegend: showlegend_bool,
-          hovertemplate:
-            "<b>%{customdata}</b><br><br>" +
-            "<extra></extra>",
-          line: {
-            color: category_color_map[d.category],
-            width: 10
-          }
-        }
-      )
-    })
     return (
       <div className={styles.contentDiv}>
-        <h1>Overview</h1>
         <div className={styles.mainChartDiv}>
-          <Plot className={styles.mainChart}
-            data={role_traces}
-            layout={
-              {
-                autosize: true,
-                hovermode: "closest",
-                showlegend: true,
-                plot_bgcolor: 'rgba(0, 0, 0, 0)',
-                paper_bgcolor: 'rgba(0, 0, 0, 0)',
-                margin: {l:150},
-                legend: {
-                  orientation: "h",
-                  xanchor: "center",
-                  yanchor: "top",
-                  x: 0.5,
-                  y: 1.3
-                },
-                xaxis: {
-                  showlines: true,
-                  linecolor: 'black',
-                  mirror : "allticks",
-                  side: "top"
-                },
-                yaxis: {
-                  showlines: true,
-                  linecolor: 'black',
-                  mirror: true,
-                  visible: false,
-                  tickfont: {size: 8}
-                }
-              }
-            }
-            config={{responsive: true}}
+          <OverViewTimeline
+            work_data = {work_data}
+            skill_data = {skill_data}
+            award_data = {award_data}
+            category_color_map = {category_color_map}
           />
         </div>
       </div>
