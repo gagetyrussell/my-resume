036709aabbdd50bf9ac3d43e590937dcc35d66c8
Gage Russell <gagetyrussell@gmail.com>
2020-09-18 19:25:16 -0500

    Revert "handle filetypes"

--- pages/tools/pivotReporter.js
+++ pages/tools/pivotReporter.js
@@ -1,80 +1,79 @@
+import router from 'umi/router';
 import React from 'react';
-import XLSX from 'xlsx';
 import { Upload, message } from 'antd';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import DataInput from '../../components/DataInput/index.js';
+import { InboxOutlined } from '@ant-design/icons';
+import * as Papa from 'papaparse';
 import PivotTableUI from 'react-pivottable/PivotTableUI';
 import 'react-pivottable/pivottable.css';
 import TableRenderers from 'react-pivottable/TableRenderers';
 import Plot from 'react-plotly.js';
 import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
-import styles from './pivotReporter.less';
+
+const { Dragger } = Upload;
 
 const PlotlyRenderers = createPlotlyRenderers(Plot);
 
-class SheetJSApp extends React.Component {
-  constructor(props) {
-    super(props);
+const divStyle = {
+  overflowX: 'scroll'
+};
+
+
+class pivotReporter extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      data: [] /* Array of Arrays e.g. [["a","b"],[1,2]] */,
+      csvfile: undefined,
+      data: []
     };
-    this.handleFile = this.handleFile.bind(this);
-    this.exportFile = this.exportFile.bind(this);
+    this.updateData = this.updateData.bind(this);
   }
-  handleFile(file /*:File*/) {
-    /* Boilerplate to set up FileReader */
-    const reader = new FileReader();
-    const rABS = !!reader.readAsBinaryString;
-    reader.onload = (e) => {
-      /* Parse data */
-      const bstr = e.target.result;
-      const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      /* Convert array of arrays */
-      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      const headers = data.shift();
-      const result = data.map(function(row) {
-          var jsonRow = {};
-          row.forEach(function(cellValue, cellIndex) {
-              jsonRow[headers[cellIndex]] = cellValue;
-          });
-          return jsonRow;
-      });
-      /* Update state */
-      this.setState({ data: result, cols: headers });
-    };
-    if (rABS) reader.readAsBinaryString(file);
-    else reader.readAsArrayBuffer(file);
+
+  handleChange = event => {
+    this.setState({
+      csvfile: event.target.files[0]
+    });
+  };
+
+  importCSV = () => {
+    const { csvfile } = this.state;
+    Papa.parse(csvfile, {
+      complete: this.updateData,
+      header: true
+    });
+  };
+
+  updateData(result) {
+    let data = result.data;
+    console.log(result.data)
+    this.setState({data: result.data});
   }
-  exportFile() {
-    /* convert state to workbook */
-    const ws = XLSX.utils.aoa_to_sheet(this.state.data);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
-    /* generate XLSX file and send to client */
-    XLSX.writeFile(wb, "sheetjs.xlsx");
-  }
+
   render() {
-    console.log(this.state.data)
+    console.log(this.state.csvfile);
     return (
-      <div>
-        <div>
-          <DataInput handleFile={this.handleFile} />
-        </div>
-        <div className={styles.pivotTable}>
-          <PivotTableUI
-              data={this.state.data}
-              cols={[]}
-              rows={[]}
-              onChange={s => this.setState(s)}
-              renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
-          />
-        </div>
+      <div style={divStyle}>
+        <input
+          className="csv-input"
+          type="file"
+          ref={input => {
+            this.filesInput = input;
+          }}
+          name="file"
+          placeholder={null}
+          onChange={this.handleChange}
+        />
+        <p />
+        <button onClick={this.importCSV}> Load Into Pivot</button>
+        <PivotTableUI
+            data={this.state.data}
+            onChange={s => this.setState(s)}
+            renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+            {...this.state}
+        />
       </div>
     );
   }
 }
 
-export default SheetJSApp;
+
+export default pivotReporter;
